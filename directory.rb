7351879ff712158fef5f1e6fe0cd5754f2f4ff8d
Andybird88 <anbird88@gmail.com>
2021-10-12 10:09:28 +0000

    eleventh commit - added number to student names

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-
-
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
-
 def print_header
 #First print a list of villans to screen.
 puts "The students of Villans Academy"
@@ -21,16 +5,41 @@ puts "------------"
 end
 
 def print(names)
-  names.each{|stud|
-    puts "#{stud[:name]} (#{:cohort} cohort)"    
+  names.each_with_index { |stud, index|
+    puts "#{index+1}. #{stud[:name]} (#{stud[:cohort]} cohort)"    
   }
 end
 
 def print_footer(names)
-#Finally we print the total
-puts "Overall, we have #{names.count} great students"
+  #Finally we print the total
+  puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  
+  puts "Please enter the names of the students"
+  puts "To finish hit return twice"
+  
+  name = gets.chomp
+  students = []
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students."
+    name = gets.chomp
+  end
+  
+  students
+  
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
+
+
+
+
+
+
+
