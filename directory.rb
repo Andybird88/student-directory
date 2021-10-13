6562169b0654a9beec6597fa856175ed90319849
Andybird88 <anbird88@gmail.com>
2021-10-13 09:34:35 +0000

    22nd commit - refactored interactive nmenu by adding methods, added @students variable

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+@students = [] #An empty array accesible by all methods
 
 def print_header
   #First print a list of villans to screen.
@@ -5,70 +6,65 @@ def print_header
   puts "------------".center(50)
 end
 
-def print(names)
+def print_student_list
   
-  length = names.length
+  length = @students.length
   index = 0
-  if names == []
+  if @students == []
     puts "No students are cuurentlyy enrolled in the academy"
   else
   while true
-    puts "#{index+1}. #{names[index][:name]} (#{names[index][:cohort]} cohort) Hobbies: #{names[index][:hobbies]}, Place of Birth: #{names[index][:placeOfBirth]}, Height: #{names[index][:height]} cm"
+    puts "#{index+1}. #{@students[index][:name]} (#{@students[index][:cohort]} cohort) Hobbies: #{@students[index][:hobbies]}, Place of Birth: #{@students[index][:placeOfBirth]}, Height: #{@students[index][:height]} cm"
     index += 1 
     length -= 1
     if length == 0
       break
     end
   end
+  end
+end
 
-
-def printBeginWithLetter(names,letter)
+def printBeginWithLetter(letter)
   puts ""
   puts "Students begining with #{letter}:"
-  names.each_with_index { |stud, index|
+  @students.each_with_index { |stud, index|
     if "#{stud[:name]}"[0] == letter
     puts "#{index+1}. #{stud[:name]} (#{stud[:cohort]} cohort)"
     end
   }
 end
 
-def nameShorterThan(names,short)
+def nameShorterThan(short)
   puts ""
   puts "Students with a name shorter than #{short} characters:"
-  names.each_with_index { |stud, index|
+  @students.each_with_index { |stud, index|
     if "#{stud[:name]}".length < 12
     puts "#{index+1}. #{stud[:name]} (#{stud[:cohort]} cohort)"
     end
   }
-end
-  end
-end
+
+end  
 
 
-def print_footer(names)
+def print_footer
   #Finally we print the total
-  if names.count == 1
-    puts "Overall, we have #{names.count} great student"
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student"
   else
-    puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
 def input_students
-  
   puts "Please enter the names of the students"
   puts "To finish hit return thrice"
   
   name = gets
   name = name[0,name.length-1]
-  students = []
   
   while !name.empty? do
-  
     puts "Enter the starting month of the students cohort"
     cohort = gets.chomp.to_s
-  
-
   
     while true
      
@@ -78,41 +74,40 @@ def input_students
       
       if cohort != "january" && cohort != "february" && cohort != "march" && cohort != "april" && cohort != "may" && cohort != "june" && cohort != "july" && cohort != "august" && cohort != "september" && cohort != "october" && cohort != "november" && cohort != "december"
         puts "please enter a valid month"
-         cohort = gets.chomp
+        cohort = gets.chomp
       else
         cohort = cohort.to_sym
         puts "Please enter a hobbie for the current student"
         hobbies = gets.chomp
-        if hobbies == ""
-          hobbies = "n/a"
-        end
-        puts "Please enter the height of the student"
-        height = gets.chomp
-        if height == ""
-          height = "n/a"
-        end
-        puts "Please enter the birth place of the student"
-        pob = gets.chomp
-        if pob == ""
-          pob = "n/a"
-        end
+      
+      if hobbies == ""
+        hobbies = "n/a"
+      end
+      puts "Please enter the height of the student"
+      height = gets.chomp
+      if height == ""
+        height = "n/a"
+      end
+      puts "Please enter the birth place of the student"
+      pob = gets.chomp
+      if pob == ""
+        pob = "n/a"
+      end
         
-        break
+      break
       end
     end
     
-    students << {name: name, cohort: cohort, hobbies: hobbies, height: height, placeOfBirth: pob }
+    @students << {name: name, cohort: cohort, hobbies: hobbies, height: height, placeOfBirth: pob }
   
-    if students.count == 1
-    puts "We have #{students.count} student."
+    if @students.count == 1
+    puts "We have #{@students.count} student."
     else
-    puts "We have #{students.count} students"
+    puts "We have #{@students.count} students"
     end
     name = gets.chomp
     
-  
   end
-  students
 end
 
 
@@ -125,30 +120,34 @@ def printbycohort(names, month)
 end
 
 def interactive_menu
-  students = []
-  
   loop do
-  #Print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" #9 as will add more items
-  
-  #Read the input and save it into a variable
-    selection = gets.chomp
-  #Do what the user has asked
-    case selection
-      when "1"
-        students = input_students #input the students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students) #Show the students
-      when "9"
-        exit #This will terminate the program
-      else
-        puts "I don't know what you mean, try again"
-    end
-  
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selction)
+  case selction
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't understand that action, try again"
   end
 end
 
