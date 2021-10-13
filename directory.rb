8fb4a1c1cc8b700b556ad850e36412b4f656d6f4
Andybird88 <anbird88@gmail.com>
2021-10-13 10:02:17 +0000

    22nd commit - added save students function and a gitignore file so it doesnt upload to git

--- directory.rb
+++ directory.rb
@@ -129,6 +129,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -144,11 +145,25 @@ def process(selction)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
       puts "I don't understand that action, try again"
   end
+end
+
+
+def save_students
+  file = File.open("students.csv", "w")
+  
+  @students.each {|student|
+    student_data = [student[:name], student[:cohort], student [:hobbies], student[:placeOfBirth], student[:height]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  }
+  file.close
 end
 
 interactive_menu
